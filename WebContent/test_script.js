752fcbb70dd958ad7f310ae5a9e5ef00eafa0306
refra <andrearefraschini@gmail.com>
2020-02-11 16:29:11 +0100

    Delete test_script.js

--- WebContent/test_script.js
+++
@@ -1,7 +0,0 @@
-
-	let paragraph = document.getElementById("par");
-	let textLine;
-	for(let i = 0; i<10; i++ ){
-		textLine = document.createTextNode("" + i);
-        paragraph.appendChild(textLine);
-	}
